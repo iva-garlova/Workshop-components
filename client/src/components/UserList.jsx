49d89d4a66ef08d1534e76a4095d46ab1126dbd2
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-09-02 16:08:33 +0300

    extract UserListItem component

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,5 +1,6 @@
 import Pagination from "./Pagination";
 import SearchForm from "./SearchForm";
+import UserListItem from "./UserListItem";
 
 export default function UserList() {
     return (
@@ -57,32 +58,7 @@ export default function UserList() {
                 </tr>
               </thead>
               <tbody>
-                {/* Table row component */}
-                <tr>
-                  <td>
-                    <img
-                      src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-                      alt="Peter's profile"
-                      className="image"
-                    />
-                  </td>
-                  <td>Peter</td>
-                  <td>Johnson</td>
-                  <td>peter@abv.bg</td>
-                  <td>0812345678</td>
-                  <td>June 28, 2022</td>
-                  <td className="actions">
-                    <button className="btn edit-btn" title="Edit">
-                      <i className="fa-solid fa-pen-to-square"></i>
-                    </button>
-                    <button className="btn delete-btn" title="Delete">
-                      <i className="fa-solid fa-trash"></i>
-                    </button>
-                    <button className="btn info-btn" title="Info">
-                      <i className="fa-solid fa-info"></i>
-                    </button>
-                  </td>
-                </tr>
+              <UserListItem />
               </tbody>
             </table>
           </div>
